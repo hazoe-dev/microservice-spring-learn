32d352df9428d4c2e2c27f9156516773efd40165
hazoe-dev <hatran.dev@gmail.com>
2026-02-07 17:43:36 +0700

    feat: create api gateway service.

---
+++ microservice-app/api-gateway/src/test/java/dev/hazoe/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hazoe.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
